09280b5044be9f00b3ecab08193ef22491c2c13e
userJulia <julia.f.19.03@gmal.com>
2021-02-18 19:58:19 +0200

    add new html and js files for task04

---
+++ tasks/04-work-with-variables.js
@@ -0,0 +1,15 @@
+"use strict"
+let admin;
+let name;
+  name = "Джон";
+  admin = name;
+alert (admin);
+
+/*
+Тема 05. Типы данных
+Что выведет этот скрипт?
+let name = "Ilya";
+  alert (`hello ${1}`); // hello 1
+  alert (`hello ${"name"}`); // hello name
+  alert (`hello ${name}`); // hello Ilya
+*/
