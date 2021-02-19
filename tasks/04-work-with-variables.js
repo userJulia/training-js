8ebac90c6f9a8687df858e72bb9904ee84ddc9fd
userJulia <julia.f.19.03@gmal.com>
2021-02-19 12:24:22 +0200

    changed quotes, removed space after function name, aligned code

--- tasks/04-work-with-variables.js
+++ tasks/04-work-with-variables.js
@@ -1,15 +1,16 @@
-"use strict"
+'use strict'
 let admin;
 let name;
-  name = "Джон";
-  admin = name;
-alert (admin);
+name = 'John';
+admin = name;
+alert(admin);
 
 /*
-Тема 05. Типы данных
-Что выведет этот скрипт?
+Topic 05. Data types
+What wiil this script output?
 let name = "Ilya";
   alert (`hello ${1}`); // hello 1
   alert (`hello ${"name"}`); // hello name
   alert (`hello ${name}`); // hello Ilya
 */
+ 
