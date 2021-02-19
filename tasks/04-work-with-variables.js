40928cae1cc9f76a087b2b6368953b34ce05c77b
userJulia <79098227+userJulia@users.noreply.github.com>
2021-02-19 13:16:00 +0200

    add new html and js files for task04 (#2)
    * add new html and js files for task04
    
    * changed quotes, removed space after function name, aligned code
    
    * changed quotes
    
    * breaked code on logical parts
    
    Co-authored-by: userJulia <julia.f.19.03@gmal.com>

---
+++ tasks/04-work-with-variables.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let admin;
+let name;
+name = 'John';
+admin = name;
+
+alert(admin);
+
+/*
+Topic 05. Data types
+What wiil this script output?
+let name = "Ilya";
+  alert (`hello ${1}`); // hello 1
+  alert (`hello ${"name"}`); // hello name
+  alert (`hello ${name}`); // hello Ilya
+*/
+ 
