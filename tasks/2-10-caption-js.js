98f1756ca9933291cfb0480a1ab0e789e6866669
userJulia <julia.f.19.03@gmal.com>
2021-02-22 12:13:56 +0200

    added new files for tasks in conditional branching chapter

---
+++ tasks/2-10-caption-js.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let officialName = prompt('What is the official name of JavaScript?');
+
+if (officialName == 'ECMAScript') {
+  alert('You are right!');
+} else {
+  alert('Do not know? ECMAScript!');
+}
---
+++ tasks/2-10-number-sign.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let number = prompt('Enter a number');
+
+if (number > 0) {
+  alert('1');
+} else if (number < 0) {
+  alert('-1');
+} else {
+  alert('0');
+}
+
