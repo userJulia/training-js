62038daeaeadc6ad334b36ce03f3e2105f3a0df4
userJulia <julia.f.19.03@gmal.com>
2021-02-24 08:09:33 +0200

    added strict equality and conditional for cansel option

--- tasks/2-11-check-login.js
+++ tasks/2-11-check-login.js
@@ -2,17 +2,17 @@
 
 let userName = prompt('Enter login');
 
-if (userName == 'Admin') {
+if (userName === 'Admin') {
   let userPassword = prompt('Enter password');
 
-  if (userPassword == 'big boss') {
+  if (userPassword === 'big boss') {
     alert('Welcome!');
-  } else if (userPassword == '') {
+  } else if (userPassword === '' || userPassword === null) {
     alert('Cancel');
   } else {
     alert('Wrong Password!');
   }
-} else if (userName == '') {
+} else if (userName === '' || userName === null) {
   alert('Cancel');
 } else {
   alert('I do not know you!');
