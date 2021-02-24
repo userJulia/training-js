82745fdbc0a953b77ea9cbdfef6c90f01dfa42ed
userJulia <julia.f.19.03@gmal.com>
2021-02-24 14:36:47 +0200

    tasks for chapter statement switch

---
+++ tasks/2-14-write-if.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const browser = prompt('Enter browser');
+
+if (browser === 'Edge') {
+  alert('You have got the Edge!');
+} else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+  alert('Okay, we support these browsers too');
+} else {
+  alert('We hope that this page looks ok!');
+}
+
+
+
+
+
+
