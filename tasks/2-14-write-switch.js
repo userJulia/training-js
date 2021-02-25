f89cab4cc0cbac75cdae222fca6c7a58322b35ad
userJulia <79098227+userJulia@users.noreply.github.com>
2021-02-25 17:09:13 +0200

    tasks for chapter statement switch (#10)
    * tasks for chapter statement switch
    
    * removed extra spaces in the end of js files
    
    Co-authored-by: userJulia <julia.f.19.03@gmal.com>

---
+++ tasks/2-14-write-switch.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const number = +prompt('Enter a number between 0 and 3');
+
+switch (number) {
+  case 0:
+    alert('You entered number 0');
+    break;
+
+  case 1:
+    alert ('You entered number 1');
+    break;
+
+  case 2:
+  case 3:
+    alert('You entered number 2 or 3');
+    break;
+}
