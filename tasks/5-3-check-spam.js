f320c02f0145ad5889f68bcaf0a26fd2d91ba3c6
userJulia <julia.f.19.03@gmal.com>
2021-03-15 13:42:16 +0200

    simplyfied function checkSpam,delited if-construction

--- tasks/5-3-check-spam.js
+++ tasks/5-3-check-spam.js
@@ -2,11 +2,7 @@
 
 function checkSpam(str) {
   str = str.toLowerCase();
-  if(str.includes('viagra') || str.includes('xxx')) {
-    return true;
-  } else {
-    return false;
-  }
+  return str.includes('viagra') || str.includes('xxx');
 }
 
 console.log(checkSpam('buy ViAgRA now'));
