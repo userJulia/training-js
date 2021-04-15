847a7b2c90a05da9a2d47e7e06d4cabb964d18c8
userJulia <julia.f.19.03@gmal.com>
2021-04-15 16:49:06 +0300

    added simple version of solution, added node_modules to gitignore file

--- tasks/5-7-filtration-unique-elem.js
+++ tasks/5-7-filtration-unique-elem.js
@@ -1,26 +1,42 @@
 'use strict';
 
 function unique(arr) {
-  let set = new Set([]);
-  for(let value of arr) {
+  const set = new Set([]);
+  for (const value of arr) {
     set.add(value);
   }
   return set;
 }
 
-let values = ["Hare", "Krishna", "Hare", "Krishna",
-"Krishna", "Krishna", "Hare", "Hare", ":-O"
+const values = [
+  'Hare',
+  'Krishna',
+  'Hare',
+  'Krishna',
+  'Krishna',
+  'Krishna',
+  'Hare',
+  'Hare',
+  ':-O',
 ];
 
-console.log(unique(values));//"Hare", "Krishna", ":-O"
+console.log(unique(values)); // "Hare", "Krishna", ":-O"
 
-//use arrow-function
-let unique1 = (arr) => {
-  let set = new Set([]);
-  for(let value of arr) {
+// simple version
+const unique0 = (arr) => {
+  const uniqueValue = Array.from(new Set(arr));
+  return uniqueValue;
+};
+
+console.log(unique0(values));// "Hare", "Krishna", ":-O"
+
+// use arrow-function
+const unique1 = (arr) => {
+  const set = new Set([]);
+  for (const value of arr) {
     set.add(value);
   }
   return set;
-}
+};
 
-console.log(unique1(values));//"Hare", "Krishna", ":-O"
+console.log(unique1(values));// "Hare", "Krishna", ":-O"
