2352d4c071b7521d6d76b874b6e1f5e1841d3e22
userJulia <julia.f.19.03@gmal.com>
2021-04-19 15:46:46 +0300

    improved code style

--- tasks/5-11-date-formatting.js
+++ tasks/5-11-date-formatting.js
@@ -1,39 +1,38 @@
 'use strict';
 
-function formatDate(date) {
-
-  let differenceDate = new Date() - date;
+const formatDate = (date) => {
+  const differenceDate = new Date() - date;
 
   if (differenceDate < 1000) {
     return 'right now';
   }
 
-  let seconds = (differenceDate / 1000);
+  const seconds = differenceDate / 1000;
 
   if (seconds < 60) {
-    return seconds + 'seconds away';
+    return `${seconds} seconds ago`;
   }
 
-  let minutes = (differenceDate / 60000);
+  const minutes = differenceDate / 60000;
   if (minutes < 60) {
-    return minutes + 'minutes away';
+    return `${minutes} minutes ago`;
   }
 
   let now = date;
-    now = [
-    '0' + now.getDate(),
-    '0' + (now.getMonth() + 1),
-    '' + now.getFullYear(),
-    '' + now.getHours(),
-    '' + now.getMinutes(),
-  ]
-  now.map(function(parts){
-    parts.slice(-2);
+  now = [
+    `${now.getDate()}`,
+    `0${(now.getMonth() + 1)}`,
+    `${now.getFullYear()}`,
+    `${now.getHours()}`,
+    `${now.getMinutes()}`,
+  ];
+  now.map((parts) => {
+    return parts.slice(-2);
   });
-  return now.slice(0, 3).join('.') + ' ' + now.slice(3).join(':');
-}
+  return `${now.slice(0, 3).join('.')} ${now.slice(3).join(':')}`;
+};
 
-console.log(formatDate(new Date(new Date - 1))); //right now
-console.log(formatDate(new Date(new Date - 30 * 1000))); //30seconds away
-console.log(formatDate(new Date(new Date - 5 * 60 * 1000))); //5minutes away
-console.log(formatDate(new Date(new Date - 86400 * 1000)));//05.04.2021 17:18
+console.log(formatDate(new Date(new Date() - 1))); // right now
+console.log(formatDate(new Date(new Date() - 30 * 1000))); // 30seconds away
+console.log(formatDate(new Date(new Date() - 5 * 60 * 1000))); // 5minutes away
+console.log(formatDate(new Date(new Date() - 86400 * 1000)));// 05.04.2021 17:18
